3d1a95e837ebc5fef989a09ec0880c3e951a3af4
sfunzbob7 <sfunzbob7@gmail.com>
2023-05-17 16:07:52 +0900

    20230517

--- 01-Terraform/local/main.tf
+++ 01-Terraform/local/main.tf
@@ -7,6 +7,16 @@ resource "local_file" "foo" {
   # ${path.module} : string interpolation 문법
   # 파일이 위치한 디렉토리 경로, main.tf
   filename = "${path.module}/foo.txt"
-  content  = "Hello World!"
+  content  = data.local_file.bar.content
+}
+
+# 입력 데이터파일 지정
+data "local_file" "bar" {
+  filename = "${path.module}/bar.txt"
+}
+
+# 데이터 출력 지정
+output "file_bar" {
+  value = data.local_file.bar
 }
 
