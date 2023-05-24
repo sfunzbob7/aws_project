50dbe4cd7bcb8bc754af9c94f8936fbe0ed7bb5e
sfunzbob7 <sfunzbob7@gmail.com>
2023-05-24 15:22:39 +0900

    20230524

---
+++ 01-Terraform/for_each/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_iam_user" "example" {
+	for_each = toset(var.user_names)
+	name		 = each.value
+}
+
+variable "user_names" {
+	description = "Create IAM users with these names"
+	type				= list(string)
+	default			= ["aws02-neo", "aws02-trinity", "aws02-morpheus"]
+}
+
+output "all_user" {
+	value 			= values(aws_iam_user.example)[*].arn
+	description = "The Name for all users"
+}
