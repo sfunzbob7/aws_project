7acb9f83077f03240fe7d07b8b49caed90257eb8
sfunzbob7 <sfunzbob7@gmail.com>
2023-06-21 13:58:04 +0900

    20230621

--- 01-Terraform/infra/ec2/bastion/backend.tf
+++ 01-Terraform/infra/ec2/bastion/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # 이전에 생성한 버킷 이름으로 변경
     bucket = "aws002-terraform-state"
-    key    = "infra/network/vpc/terraform.tfstate"
+    key    = "infra/ec2/bastion/terraform.tfstate"
     region = "ap-northeast-2"
 
     # 이전에 생성한 다이나모DB 테이블 이름으로 변경
